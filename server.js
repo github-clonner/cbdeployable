3959aef8ad4fa70fb9c57519a42970723ab1dcd5
chrismhale <chrismhale@gmail.com>
2016-10-12 14:26:52 -0400

    outputting the payload

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ handler.on('push', function (event) {
 	console.log('Received a push event for %s to %s',
 		event.payload.repository.name,
 		event.payload.ref);
+	console.log(event.payload);
 });
 
 handler.on('issues', function (event) {
