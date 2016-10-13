334c98c3bb7b5c64476c346364ba96ccbc0f28bd
chrismhale <chrismhale@gmail.com>
2016-10-13 15:46:42 -0400

    added test file and ansible module

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require('http'),
 	createHandler = require('./lib/webhook-handler'),
 	handler = createHandler({ path: '/webhook', secret: 'myhashsecret' }),
-	//doDeployment = require('./lib/ansible-handler'),
-	port = process.env.PORT || 8080;
+	ansible = require('./lib/ansible-handler'),
+	port = process.env.PORT || 8080,
+	deploy;
 
 http.createServer(function (req, res) {
 	handler(req, res, function (err) {
@@ -15,18 +16,20 @@ handler.on('error', function (err) {
 	console.error('Error:', err.message);
 });
 
+deploy.on('success', function (options) {
+	console.log('success', options.playbook, options.vars);
+});
+
 handler.on('push', function (event) {
 	console.log('Received a push event for %s to %s',
 		event.payload.repository.name,
 		event.payload.ref);
 
-	console.log(event.payload.toJSON());
-
-	// deploy({
-	// 	playbook: '',
-	// 	vars: {
-	// 		env: ''
-	// 	}
-	// })
+	deploy = ansible.deploy({
+		playbook: 'temp',
+		vars: {
+			env: 'dev'
+		}
+	});
 
 });
