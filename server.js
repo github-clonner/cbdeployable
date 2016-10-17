05096dd62d5e2908c10586f07f2572ef359af534
chrismhale <chrismhale@gmail.com>
2016-10-17 10:03:23 -0400

    added test file and ansible module

--- server.js
+++ server.js
@@ -3,40 +3,41 @@ var http = require('http'),
 	ansibleHandler = require('./lib/ansible-handler'),
 	Emitter = require('tiny-emitter'),
 	emitter = new Emitter(),
-	port = process.env.PORT || 8080;
-
-http.createServer(function (req, res) {
-	var hookRouter = {
+	port = process.env.PORT || 8080,
+	hookRouter = {
 		path: '/webhook',
 		secret: 'myhashsecret'
-	}
+	};
 
-	webhookHandler.initialize(emitter, hookRouter, function(obj) {
-		obj.handler(req, res, function (err) {
+http.createServer(function (req, res) {
+	webhookHandler.initialize(emitter, hookRouter, function(o) {
+		o.handler(req, res, function (err) {
 			res.statusCode = 404;
 			res.end('no such location');
 		});
 	});
-
 }).listen(port);
 
-emitter.on('error', function (err) {
-	console.error('Error: %s', err);
-});
-
-emitter.on('deploy-success', function (msg) {
-	console.error('Success: %s', msg);
-});
 
 emitter.on('push', function (event) {
 	console.log('Received a push event for %s to %s', event.payload.repository.name, event.payload.ref);
 
-	ansibleHandler.initialize(emitter, function() {
-		ansibleHandler.deploy({
+	ansibleHandler.initialize(emitter, function(o) {
+		o.deploy({
 			playbook: 'temp',
 			vars: {
 				env: 'dev'
 			}
 		});
 	});
+});
+
+
+emitter.on('deploy-success', function (msg) {
+	console.error('Success: ', msg);
+});
+
+
+emitter.on('error', function (err) {
+	console.error('Error: %s', err);
 });
